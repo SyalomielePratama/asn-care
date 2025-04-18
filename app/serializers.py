f8c5b3c67b48cb2d00ab88718525cfe9f0fc2cdf
SyalomielePratama <pratamasyalomiele@gmail.com>
2025-04-18 21:16:35 +0700

    fix bug (gatau tiba tiba bisa)

--- app/serializers.py
+++ app/serializers.py
@@ -15,7 +15,7 @@ class PegawaiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pegawai
         fields = '__all__'
-        read_only_fields = ('id_pegawai','umur', 'waktu_kenaikan_pangkat', 'waktu_kenaikan_gaji', 'namaPegawai')
+        read_only_fields = ('id_pegawai','umur', 'waktu_kenaikan_pangkat', 'waktu_kenaikan_gaji')
 
     def get_waktu_kenaikan_pangkat(self, obj):
         tahun, bulan = obj.waktu_kenaikan_pangkat()
